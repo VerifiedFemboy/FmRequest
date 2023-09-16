6fbdc9f18d616980035c59444d6914f020e7efb0
verifiedfemboy <maciekplaygamesyt@gmail.com>
2023-09-16 15:27:32 +0200

    Updated Classes

--- src/main/java/me/verifiedfemboy/fm/FMUtils.java
+++ src/main/java/me/verifiedfemboy/fm/FMUtils.java
@@ -1,6 +1,29 @@
 package me.verifiedfemboy.fm;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
 public class FMUtils {
 
+
+    public static Gson getJson(){
+        return new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    public static String sendRequest(String url) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        HttpResponse response = httpClient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        return EntityUtils.toString(entity);
+    }
 
 }
--- src/main/java/me/verifiedfemboy/fm/track/recentTrack.java
+++ src/main/java/me/verifiedfemboy/fm/track/recentTrack.java
@@ -2,8 +2,8 @@ package me.verifiedfemboy.fm.track;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import me.verifiedfemboy.Utils;
 import me.verifiedfemboy.fm.API;
+import me.verifiedfemboy.fm.FMUtils;
 
 import java.io.IOException;
 
@@ -14,7 +14,7 @@ public class recentTrack{
 
     public recentTrack(String userName, API api) throws IOException {
         String url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + userName + "&api_key=" + api.getAPI_KEY() + "&format=json";
-        jsonObject = Utils.getJson().fromJson(Utils.sendRequest(url), JsonObject.class);
+        jsonObject = FMUtils.getJson().fromJson(FMUtils.sendRequest(url), JsonObject.class);
     }
     public JsonObject recentTracks(){
         return jsonObject.get("recenttracks").getAsJsonObject();
